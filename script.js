d2fefbcd8c9d46e07fe0315b9d62c18183ddc818
6395728950 <161918761+6395728950@users.noreply.github.com>
2024-11-18 14:48:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+document.getElementById("button").addEventListner("click",function(){
+	// get the data
+	const name = document.getElementById("name");
+	const year = document.getElementById("year");
+
+	// update url
+	if(name && year){
+		const url = `https://localhost:8080/?name=${name}&year=${year}`;
+		document.getElementById("url").textContent = url;
+	}
+	else{
+		alert("plase filled all the details");
+	}
+	
+})
